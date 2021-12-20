c8e9334b9da16d5b7dd60011220b05a9547a7b1f
Kerstin Huppenbauer <k.huppenbauer@web.de>
2021-12-20 21:10:52 +0100

    TASK: Add Access-Control-Allow Headers

--- functions/search.js
+++ functions/search.js
@@ -11,12 +11,28 @@ exports.handler = async (event) => {
     const client = algoliasearch(appId, searchKey);
     const index = client.initIndex(algoliaIndex);
     const res = await index.search('', query);
+    const headers = {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE, PUT',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    }
+    return {
+      statusCode: 200,
+      headers,
+      body: JSON.stringify(res),
+    };
+  }
+  if (event.httpMethod === 'OPTIONS') {
     return {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE, PUT',
+        'Access-Control-Allow-Headers': 'Content-Type',
       },
-      body: JSON.stringify(res),
+      body: "Ok"
     };
   }
   return {
